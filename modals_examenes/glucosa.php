85105ab32daec86ed16368984097f8b7f10d9c23
OscarGonzalez14 <oscargonzalez815@gmail.com>
2021-10-22 10:50:10 -0600

    update ingreso resultador

--- modals_examenes/glucosa.php
+++ modals_examenes/glucosa.php
@@ -1,3 +1,19 @@
-
-        <td colspan="100" style="text-align: center;" class="bg-light"><b><span style="color: blue">PACIENTE:</span>&nbsp;<span id="paciente_exa" class="paciente_exa"></span></b></td>
     
+<div>
+  <div class="form-row">
+    <div class="form-group  col-md-12">
+      <label for="">Resultado</label>
+      <input type="number" class="form-control" id="resultado_glucosa" style="text-align: right;" autofocus>
+      <span class="input-group-append">
+          <button type="button" class="btn btn-info btn-flat" onClick="GuardarGlucosa();">mg/dl</button>
+      </span>
+    </div>
+
+    <div class="colm-12">
+      <div class="form-group col-md-12">
+        <label for="inputEmail4">Observaciones</label>
+        <input type="text" class="form-control" id="observacione_glucosa" required="" style="text-align: right;">
+      </div>
+    </div>
+  </div>
+</div>   
